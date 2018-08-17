6fe1f2b47b867c75966c5aa4709b1dbd954b73e7
Robby Gunawan Sutanto <rgunawans@hotmail.com>
2018-08-17 09:33:42 +0700

    add button control, change color when mouse over the control and leave the button

---
+++ src/AuroraControls/aButton.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace AuroraControls
+{
+    public class aButton:Button
+    {
+        #region Member Variables
+        Color focusColor = Color.White;
+        Color unfocusColor = Color.White;
+        Color focusTextColor = Color.Black;
+        Color unfocusTextColor = Color.Black;
+        #endregion
+
+        public aButton()
+        {
+            this.MouseEnter += AButton_MouseEnter;
+            this.MouseLeave += AButton_MouseLeave;
+        }
+
+        private void AButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.BackColor = unfocusColor;
+            this.focusTextColor = unfocusTextColor;
+        }
+
+        private void AButton_MouseEnter(object sender, EventArgs e)
+        {
+            this.BackColor = focusColor;
+            this.ForeColor = focusTextColor;
+        }
+
+        #region User Defined Properties
+        [Browsable(true)]
+        [Category("Extended Properties")]
+        [Description("Set color when focused")]
+        [DisplayName("Focused Color")]
+        public Color FocusedColor
+        {
+            get
+            {
+                return this.focusColor;
+            }
+            set
+            {
+                this.focusColor = value;
+
+            }
+        }
+        [Browsable(true)]
+        [Category("Extended Properties")]
+        [Description("Set color when lost focus or leave the control")]
+        [DisplayName("unFocused Color")]
+        public Color unFocusedColor
+        {
+            get
+            {
+                return this.unfocusColor;
+            }
+            set
+            {
+                this.unfocusColor = value;
+
+            }
+        }
+        [Browsable(true)]
+        [Category("Extended Properties")]
+        [Description("Set text color when focused")]
+        [DisplayName("Focused Color")]
+        public Color FocusedTextColor
+        {
+            get
+            {
+                return this.focusTextColor;
+            }
+            set
+            {
+                this.focusTextColor = value;
+
+            }
+        }
+        [Browsable(true)]
+        [Category("Extended Properties")]
+        [Description("Set text color when lost focus or leave the control")]
+        [DisplayName("unFocused Color")]
+        public Color unFocusedTextColor
+        {
+            get
+            {
+                return this.unfocusTextColor;
+            }
+            set
+            {
+                this.unfocusTextColor = value;
+
+            }
+        }
+        #endregion
+    }
+}
